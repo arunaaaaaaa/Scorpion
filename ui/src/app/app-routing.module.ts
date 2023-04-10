3e58bb30d7af3b15b3337cdaa9aed8f844075385
Ajay Pranav <ajaypranav1999@gmail.com>
2023-04-10 17:07:08 -0400

    home page

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
-  {path:'',component:LoginComponent}
+  {path:'',component:LoginComponent},
+  {path:'home',component:HomePageComponent}
 ];
 
 @NgModule({
